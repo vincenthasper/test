938c7ea54b0346795a6931ab8f417a946a329288
Vincent Hasper <125301365+vincenthasper@users.noreply.github.com>
2024-04-29 23:37:47 +0200

    Update colors.sh

--- foot/colors.sh
+++ foot/colors.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Function to print colored blocks using ANSI 256-color codes
 print_color_blocks() {
     for ((i = $1; i < $2; i++)); do
         printf "\e[48;5;${i}m   \e[0m"
@@ -7,5 +8,8 @@ print_color_blocks() {
     echo
 }
 
+# Print colored blocks from color 0 to 7
 print_color_blocks 0 8
+
+# Print colored blocks from color 8 to 15
 print_color_blocks 8 16
